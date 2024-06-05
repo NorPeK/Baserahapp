e7f2e8eaa80de2f27042714ea06b19ffeb730e0b
NorPeK <norpek@hotmail.co.uk>
2024-06-05 04:08:54 +0300

    navigation bar fix

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid ? await Firebase.initializeApp(
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
       options: const FirebaseOptions(
           apiKey: 'REMOVED_PROJECT_Key',
           appId: 'REMOVED_PROJECT_ID',
           messagingSenderId: 'REMOVED_PROJECT_SENT_ID',
-          projectId: 'REMOVED_PROJECT_MESSAGE')
-  ) : await Firebase.initializeApp();
+          projectId: 'REMOVED_PROJECT_MESSAGE'))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -36,7 +37,6 @@ class MyApp extends StatelessWidget {
         '/profile': (context) => const ProfilePage(),
         '/editProfile': (context) => const editProfilePage(),
       },
-
     );
   }
 }
@@ -59,9 +59,30 @@ class AuthWrapper extends StatelessWidget {
   }
 }
 
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0; // To track the selected tab
+
+  void _onItemTapped(int index) {
+    if (index == _selectedIndex) {
+      // Do nothing if the selected index is the same as the current index
+      return;
+    }
+    setState(() {
+      _selectedIndex = index;
+    });
+    if (index == 0) {
+      Navigator.pushReplacementNamed(context, '/home');
+    } else if (index == 1) {
+      Navigator.pushReplacementNamed(context, '/profile');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,12 +91,6 @@ class HomePage extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: const Text('Home'),
         actions: const [
-          /*
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
-          ),
-          */
           CircleAvatar(
             backgroundImage: AssetImage('assets/profile.jpg'), // Ensure to add a placeholder image
           ),
@@ -94,39 +109,8 @@ class HomePage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            /*
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                  ),
-                  child: const Text('Live User Location'),
-                ),
-                const SizedBox(width: 10),
-                TextButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                  ),
-                  child: const Text('User Location logs'),
-                ),
-              ],
-            ),
-            */
             SizedBox(height: 20),
-            //const ProgressCard(),
             SizedBox(height: 20),
-            /*
-            const Text(
-              'Categories',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-             */
             SizedBox(height: 10),
             CategoriesGrid(),
           ],
@@ -138,85 +122,17 @@ class HomePage extends StatelessWidget {
             icon: Icon(Icons.home),
             label: 'Home',
           ),
-          /*
           BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle),
-            label: '',
-          ),
-           */
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
+            icon: Icon(Icons.person),
             label: 'Profile',
           ),
         ],
-        onTap: (index) {
-          if (index == 0) {
-            Navigator.pushNamed(context, '/home');
-          } else if (index == 1) {
-            Navigator.pushNamed(context, '/profile');
-          }
-        },
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
 }
-// WE CAN REMOVE THIS CLASS
-/*
-class ProgressCard extends StatelessWidget {
-  const ProgressCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: const Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Daily progress',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 12,
-                      backgroundImage: AssetImage('assets/avatar1.jpg'),
-                    ),
-                    SizedBox(width: 5),
-                    CircleAvatar(
-                      radius: 12,
-                      backgroundImage: AssetImage('assets/avatar2.jpg'),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text('Here you can see your daily task'),
-            SizedBox(height: 10),
-            LinearProgressIndicator(
-              value: 0.76,
-              color: Colors.blue,
-              backgroundColor: Colors.grey,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
-
 
 class CategoriesGrid extends StatelessWidget {
   const CategoriesGrid({super.key});
@@ -238,82 +154,10 @@ class CategoriesGrid extends StatelessWidget {
           title: 'Click Here to See The logs history',
           icon: Icons.book,
         ),
-
       ],
     );
   }
 }
-
-/*
-// WE CAN REMOVE THIS CLASS
-class CategoryCard extends StatelessWidget {
-  final String title;
-  final int newItems;
-  final int progress;
-  final int total;
-  final Color color;
-
-  const CategoryCard({
-    super.key,
-    required this.title,
-    required this.newItems,
-    required this.progress,
-    required this.total,
-    required this.color,
-  });
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  '$newItems New',
-                  style: TextStyle(
-                    color: color,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const CircleAvatar(
-                  radius: 15,
-                  backgroundImage: AssetImage('assets/category_icon.jpg'), // Ensure to add a placeholder image
-                ),
-              ],
-            ),
-            const Spacer(),
-            Text(
-              title,
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const Spacer(),
-            LinearProgressIndicator(
-              value: progress / total,
-              color: color,
-              backgroundColor: Colors.grey,
-            ),
-            const SizedBox(height: 5),
-            Text('$progress/$total'),
-          ],
-        ),
-      ),
-    );
-  }
-}
-*/
 
 class SimpleCategoryCard extends StatelessWidget {
   final String title;
@@ -357,5 +201,3 @@ class SimpleCategoryCard extends StatelessWidget {
     );
   }
 }
-
-
