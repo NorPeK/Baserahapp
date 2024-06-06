e8a4ec94bb80dfe3aac7b7b77914533d6fa4b708
Hassan Zayer <eng.halzayer@gmail.com>
2024-06-07 01:04:34 +0300

    1

--- lib/logs.dart
+++ lib/logs.dart
@@ -22,6 +22,8 @@ class _LogsPageState extends State<LogsPage> {
     DatabaseEvent event = await _databaseReference.once();
     setState(() {
       _logs = Map<String, dynamic>.from(event.snapshot.value as Map);
+      _logs['waterLevel_sensor'] =
+          _logs.remove('current_WaterLevel_sensor_reading');
     });
   }
 
@@ -30,19 +32,52 @@ class _LogsPageState extends State<LogsPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Logs History'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
       body: _logs.isEmpty
           ? const Center(child: CircularProgressIndicator())
           : ListView.builder(
-        itemCount: _logs.length,
-        itemBuilder: (context, index) {
-          String key = _logs.keys.elementAt(index);
-          return ListTile(
-            title: Text(key),
-            subtitle: Text(_logs[key].toString()),
-          );
-        },
-      ),
+              itemCount: _logs.length,
+              itemBuilder: (context, index) {
+                String key = _logs.keys.elementAt(index);
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  child: Card(
+                    child: ListTile(
+                      title: Text(
+                        _formatTitle(key),
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        _logs[key].toString(),
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
     );
+  }
+
+  String _formatTitle(String key) {
+    switch (key) {
+      case 'waterLevel_sensor':
+        return 'Water Level Sensor ';
+      case 'current_latitude':
+        return 'Current Latitude';
+      case 'current_longitude':
+        return 'Current Longitude';
+      case 'current_distance_sensor_reading':
+        return 'Distance Sensor ';
+      default:
+        return key;
+    }
   }
 }
